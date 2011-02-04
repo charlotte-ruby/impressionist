ec1b40487dd985277c0655de74f3f9010715cabf
cowboycoded <john.mcaliley@gmail.com>
2011-02-04 10:13:27 -0500

    simply reopen ApplicationController instead of using include/extend.  As long as the file is not named "application_controller.rb", then it will load.  Rails does not load files from engine /app that have the same name as files in container application /app dir

--- lib/impressionist/engine.rb
+++ lib/impressionist/engine.rb
@@ -2,15 +2,7 @@ require "impressionist"
 require "rails"
 
 module Impressionist
-  class Engine < Rails::Engine
-    
-    initializer 'impressionist.controller' do
-      ActiveSupport.on_load(:action_controller) do
-         include ImpressionistController::InstanceMethods
-         extend ImpressionistController::ClassMethods
-      end
-    end
-    
+  class Engine < Rails::Engine    
     initializer 'impressionist.extend_ar' do
       ActiveRecord::Base.extend Impressionist::Impressionable
     end
