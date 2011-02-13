0348a2a9c0495974074bb02c3960cc2a1531743d
cowboycoded <john.mcaliley@gmail.com>
2011-02-13 14:16:12 -0500

    change back to module.  causing problems in dev with reopen implementation

--- lib/impressionist/engine.rb
+++ lib/impressionist/engine.rb
@@ -3,8 +3,15 @@ require "rails"
 
 module Impressionist
   class Engine < Rails::Engine    
-    initializer 'impressionist.extend_ar' do
+    initializer 'impressionist.extend_ar' do |app|
       ActiveRecord::Base.extend Impressionist::Impressionable
+    end
+    
+    initializer 'impressionist.controller' do
+      ActiveSupport.on_load(:action_controller) do
+        include ImpressionistController::InstanceMethods
+        extend ImpressionistController::ClassMethods
+      end
     end
   end
 end
