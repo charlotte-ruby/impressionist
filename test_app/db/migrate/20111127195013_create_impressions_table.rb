5e0319f646bdde7290e84ebedd431a527aff4567
Erik Michaels-Ober <sferik@gmail.com>
2012-03-06 15:24:00 -0800

    Update migration

--- test_app/db/migrate/20111127195013_create_impressions_table.rb
+++ test_app/db/migrate/20111127195013_create_impressions_table.rb
@@ -8,10 +8,10 @@ class CreateImpressionsTable < ActiveRecord::Migration
       t.string :action_name
       t.string :view_name
       t.string :request_hash
-      t.string :session_hash
       t.string :ip_address
-      t.string :message
-      t.string :referrer
+      t.string :session_hash
+      t.text :message
+      t.text :referrer
       t.timestamps
     end
     add_index :impressions, [:impressionable_type, :impressionable_id, :request_hash], :name => "poly_request_index", :unique => false
