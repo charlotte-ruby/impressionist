8b4ecc2a2e31e76d6fa9d637e019584af6cbf665
Erik Michaels-Ober <sferik@gmail.com>
2012-03-06 15:24:13 -0800

    Add simplecov

--- test_app/spec/spec_helper.rb
+++ test_app/spec/spec_helper.rb
@@ -1,5 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+require 'simplecov'
+SimpleCov.start 'rails'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
--- test_app/test/test_helper.rb
+++ test_app/test/test_helper.rb
@@ -1,4 +1,6 @@
 ENV["RAILS_ENV"] = "test"
+require 'simplecov'
+SimpleCov.start 'rails'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
