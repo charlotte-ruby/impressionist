cdcb2a78c5e4bfbe76b54ea9392e3343e6cb54df
Erik Michaels-Ober <sferik@gmail.com>
2012-03-06 15:28:41 -0800

    Use be_true instead of be true for cleaner output

--- test_app/spec/rails_generators/rails_generators_spec.rb
+++ test_app/spec/rails_generators/rails_generators_spec.rb
@@ -11,11 +11,11 @@ describe Impressionist do
     File.delete("#{migrations_dir}/#{impressions_migration}") unless impressions_migration.blank?
     generator_output = systemu("rails g impressionist")[1]
     migration_name = generator_output.split("migrate/")[1].strip
-    Dir.entries(migrations_dir).include?(migration_name).should be true
+    Dir.entries(migrations_dir).include?(migration_name).should be_true
   end
 
   it "should run the migration created in the previous spec" do
     migrate_output = systemu("rake db:migrate RAILS_ENV=test")
-    migrate_output[1].include?("CreateImpressionsTable: migrated").should be true
+    migrate_output[1].include?("CreateImpressionsTable: migrated").should be_true
   end
 end
