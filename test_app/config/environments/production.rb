a357d0123fbc33fcd71cb52b42050c4de1dbbe6c
Ryan Bennick <ryan@320ny.com>
2013-05-30 14:28:46 -0600

    Fixed whitelist_attributes, config.whiny_nils, and config.secret_key_base deprecation warnings

--- test_app/config/environments/production.rb
+++ test_app/config/environments/production.rb
@@ -64,4 +64,6 @@ TestApp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  config.eager_load = true
 end
