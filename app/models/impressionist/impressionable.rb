32986ed50ad69b4affe8cba5e80e9f21e52afdb4
Ryan Bennick <ryan@320ny.com>
2013-05-29 22:51:08 -0600

    Changed Rails versions, removed attr_accessible, and changed count to what Rails4 requires

--- app/models/impressionist/impressionable.rb
+++ app/models/impressionist/impressionable.rb
@@ -31,7 +31,7 @@ module Impressionist
     def impressionist_count(options={})
       options.reverse_merge!(:filter=>:request_hash, :start_date=>nil, :end_date=>Time.now)
       imps = options[:start_date].blank? ? impressions : impressions.where("created_at>=? and created_at<=?",options[:start_date],options[:end_date])
-      options[:filter] == :all ? imps.count : imps.count(options[:filter], :distinct => true)
+      options[:filter] == :all ? imps.count : imps.select(options[:filter]).distinct.count
     end
 
     def update_impressionist_counter_cache
