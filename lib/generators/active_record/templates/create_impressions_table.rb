2e939b3c28919d2de911fa5f6a87e2c522694d85
yfxie <yfxie@me.com>
2014-06-03 11:56:22 +0800

    change impressionable_id type to string
    I think it should use string type.
    In case, I define to_param on model
    and /posts/this-is-my-permalink will result in 0 value saved in column.
    It work well in any case if use string type.

--- lib/generators/active_record/templates/create_impressions_table.rb
+++ lib/generators/active_record/templates/create_impressions_table.rb
@@ -2,7 +2,7 @@ class CreateImpressionsTable < ActiveRecord::Migration
   def self.up
     create_table :impressions, :force => true do |t|
       t.string :impressionable_type
-      t.integer :impressionable_id
+      t.string :impressionable_id
       t.integer :user_id
       t.string :controller_name
       t.string :action_name
