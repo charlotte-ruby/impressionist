569f0eed9b63030e4843f07611fb4cd7b5cb5e49
Lin Jen-Shin <godfat@godfat.org>
2014-12-22 19:31:14 +0800

    Workaround for squeel, fixes #178
    Workaround squeel bug:
    https://github.com/activerecord-hackery/squeel/issues/355

--- app/models/impressionist/impressionable.rb
+++ app/models/impressionist/impressionable.rb
@@ -42,7 +42,7 @@ module Impressionist
       # Count all distinct impressions unless the :all filter is provided.
       distinct = options[:filter] != :all
       if Rails::VERSION::MAJOR == 4
-        distinct ? imps.select(options[:filter]).distinct.count : imps.count
+        distinct ? imps.distinct.count(options[:filter]) : imps.count
       else
         distinct ? imps.count(options[:filter], :distinct => true) : imps.count
       end
