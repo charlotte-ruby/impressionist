d1d6779ad7b1e244be20126fa050d18a2f924568
Gabriel Bellon <gabriel1490@gmail.com>
2017-10-25 16:34:30 -0200

    Change depency strategy for Mongoid

--- lib/impressionist/models/mongoid/impressionist/impressionable.rb
+++ lib/impressionist/models/mongoid/impressionist/impressionable.rb
@@ -1,26 +1,35 @@
 module Impressionist
   module Impressionable
+    extend ActiveSupport::Concern
 
-  # extends AS::Concern
-  include Impressionist::IsImpressionable
+    module InstanceMethods
+      # Overides impressionist_count in order to provide mongoid compability
+      def impressionist_count(options={})
 
-    # Overides impressionist_count in order to provide mongoid compability
-    def impressionist_count(options={})
+        # Uses these options as defaults unless overridden in options hash
+        options.reverse_merge!(:filter => :request_hash, :start_date => nil, :end_date => Time.now)
 
-      # Uses these options as defaults unless overridden in options hash
-      options.reverse_merge!(:filter => :request_hash, :start_date => nil, :end_date => Time.now)
-
-      # If a start_date is provided, finds impressions between then and the end_date.
-      # Otherwise returns all impressions
-      imps = options[:start_date].blank? ? impressions :
-        impressions.between(created_at: options[:start_date]..options[:end_date])
+        # If a start_date is provided, finds impressions between then and the end_date.
+        # Otherwise returns all impressions
+        imps = options[:start_date].blank? ? impressions :
+          impressions.between(created_at: options[:start_date]..options[:end_date])
 
 
-      # Count all distinct impressions unless the :all filter is provided
-      distinct = options[:filter] != :all
-      distinct ? imps.where(options[:filter].ne => nil).distinct(options[:filter]).count : imps.count
+        # Count all distinct impressions unless the :all filter is provided
+        distinct = options[:filter] != :all
+        distinct ? imps.where(options[:filter].ne => nil).distinct(options[:filter]).count : imps.count
+      end
     end
 
+    module ClassMethods
+      def is_impressionable(options={})
+        has_many :impressions, 
+                 as: :impressionable, 
+                 dependent: :delete
+
+        @impressionist_cache_options = options
+      end
+    end
   end
 end
 
