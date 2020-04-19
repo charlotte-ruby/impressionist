945290f05d5ac063e14a8adb8464ce2b8f9befe1
Matt McMahand <matt@invalid8.com>
2020-04-18 23:58:06 -0400

    rubocop

--- spec/rails_toggle_spec.rb
+++ spec/rails_toggle_spec.rb
@@ -3,31 +3,29 @@
 require 'spec_helper'
 require 'impressionist/rails_toggle'
 
-describe Impressionist do
-  describe Impressionist::RailsToggle do
-    let(:toggle) { Impressionist::RailsToggle.new }
+describe Impressionist::RailsToggle do
+  let(:toggle) { Impressionist::RailsToggle.new }
 
-    context 'when using rails < 4' do
-      it 'will be included' do
-        stub_const('::Rails::VERSION::MAJOR', 3)
+  context 'when using rails < 4' do
+    it 'will be included' do
+      stub_const('::Rails::VERSION::MAJOR', 3)
 
-        expect(toggle.should_include?).to be_truthy
-      end
-
-      it 'will not be included when strong parameters is defined' do
-        stub_const('::Rails::VERSION::MAJOR', 3)
-        stub_const('StrongParameters', Module.new)
-
-        expect(toggle.should_include?).to be_falsy
-      end
+      expect(toggle.should_include?).to be_truthy
     end
 
-    context 'when using rails >= 4' do
-      it 'will not be included' do
-        stub_const('::Rails::VERSION::MAJOR', 4)
+    it 'will not be included when strong parameters is defined' do
+      stub_const('::Rails::VERSION::MAJOR', 3)
+      stub_const('StrongParameters', Module.new)
 
-        expect(toggle.should_include?).to be_falsy
-      end
+      expect(toggle.should_include?).to be_falsy
+    end
+  end
+
+  context 'when using rails >= 4' do
+    it 'will not be included' do
+      stub_const('::Rails::VERSION::MAJOR', 4)
+
+      expect(toggle.should_include?).to be_falsy
     end
   end
 end
