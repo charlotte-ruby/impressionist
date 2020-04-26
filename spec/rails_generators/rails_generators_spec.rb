7e1df3786070f06a707b526d2f26449e87d74e5b
Matt McMahand <matt@invalid8.com>
2020-04-25 21:47:33 -0400

    update rubocop

--- spec/rails_generators/rails_generators_spec.rb
+++ spec/rails_generators/rails_generators_spec.rb
@@ -7,7 +7,7 @@ describe Impressionist, :migration do
   fixtures :articles, :impressions, :posts, :profiles
   it "deletes existing migration and generate the migration file" do
     pending
-    migrations_dir = "#{Rails.root}/db/migrate"
+    migrations_dir = Rails.root.join('db/migrate')
     impressions_migration = Dir.entries(migrations_dir).grep(/impressions/)[0]
     File.delete("#{migrations_dir}/#{impressions_migration}") if impressions_migration.present?
     generator_output = systemu("rails g impressionist")[1]
