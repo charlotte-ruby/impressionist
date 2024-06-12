7a1c80dfb27b7cd02d545748b0e8e15090034249
Tomoya Yoshida <ihatov08@gmail.com>
2024-06-12 16:46:00 +0900

    Use ActiveRecord.timestamped_migrations when rails major version is 7 or higher

--- lib/generators/active_record/impressionist_generator.rb
+++ lib/generators/active_record/impressionist_generator.rb
@@ -7,7 +7,7 @@ module ActiveRecord
       # FIX, why is this implementing rails behaviour?
       def self.next_migration_number(dirname)
         sleep 1
-        if ActiveRecord::Base.timestamped_migrations
+        if timestamped_migrations?
           Time.now.utc.strftime("%Y%m%d%H%M%S")
         else
           "%.3d" % (current_migration_number(dirname) + 1)
@@ -16,6 +16,18 @@ module ActiveRecord
 
       def create_migration_file
         migration_template 'create_impressions_table.rb.erb', 'db/migrate/create_impressions_table.rb'
+      end
+
+      class << self
+        private
+
+        def timestamped_migrations?
+          if Rails::VERSION::MAJOR >= 7
+            ActiveRecord.timestamped_migrations
+          else
+            ActiveRecord::Base.timestamped_migrations
+          end
+        end
       end
     end
   end
